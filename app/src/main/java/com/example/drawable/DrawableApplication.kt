d88c83f226e4231314eb421fe94b40559b60e05b
lissaramos <lissa13x@gmail.com>
2024-03-20 04:52:41 -0600

    Merged Database code, working on getting jetpack composable to work with the code

--- app/src/main/java/com/example/drawable/DrawableApplication.kt
+++ app/src/main/java/com/example/drawable/DrawableApplication.kt
@@ -5,24 +5,24 @@ import androidx.room.Room
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 
-//class DrawableApplication  : Application() {
-//    //coroutine scope tied to the application lifetime which we can run suspend functions in
-//    val scope = CoroutineScope(SupervisorJob())
-//
-//    //get a reference to the DB singleton
-//    //val db by lazy {WeatherDatabase.getDatabase(applicationContext)}
-//    val db by lazy {
-//        Room.databaseBuilder(
-//            applicationContext,
-//            DrawingDatabase::class.java,
-//            "drawingpaths_database"
-//        ).build()}
-//
-//    //create our repository singleton, using lazy to access the DB when we need it
-//    val drawingRepository by lazy {DrawingRepository(scope, db.drawingDao(), applicationContext)}
-//}
-//
-//
-//
-//
-//
+class DrawableApplication  : Application() {
+    //coroutine scope tied to the application lifetime which we can run suspend functions in
+    val scope = CoroutineScope(SupervisorJob())
+
+    //get a reference to the DB singleton
+    //val db by lazy {WeatherDatabase.getDatabase(applicationContext)}
+    val db by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            DrawingDatabase::class.java,
+            "drawingpaths_database"
+        ).build()}
+
+    //create our repository singleton, using lazy to access the DB when we need it
+    val drawingRepository by lazy {DrawingRepository(scope, db.drawingDao(), applicationContext)}
+}
+
+
+
+
+
