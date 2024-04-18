93090f8f3a5605a110ef839a4789c65e7bce485c
lissaramos <lissa13x@gmail.com>
2024-04-17 21:36:55 -0600

    Made progress on login and register page

--- app/src/main/java/com/example/drawable/LoginPage.kt
+++ app/src/main/java/com/example/drawable/LoginPage.kt
@@ -2,8 +2,10 @@ package com.example.drawable
 
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -17,6 +19,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -30,6 +33,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -43,9 +50,12 @@ class LoginPage : Fragment() {
 
     @Composable
     fun Login(){
-        Column(horizontalAlignment = Alignment.CenterHorizontally){
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
+            Logo()
+            Spacer(modifier = Modifier.height(10.dp))
             Text(
-                text = "Sign In",
+                text = "Log in",
                 style = TextStyle(
                     color = Color.Black,
                     fontSize = 28.sp,
@@ -58,7 +68,8 @@ class LoginPage : Fragment() {
             PasswordTextField()
             Spacer(modifier = Modifier.height(15.dp))
 
-            Button(onClick = { }){
+            Button(onClick = { },
+                colors = ButtonDefaults.buttonColors(Color(0xFF6C80E8))){
                 Text("Login")
             }
 
@@ -68,7 +79,8 @@ class LoginPage : Fragment() {
                 Text("Don't have an account? ")
                 Spacer(modifier = Modifier.width(10.dp))
                 Button(onClick = { },
-                    modifier = Modifier.background(Color.Transparent)){
+                    modifier = Modifier.background(Color.Transparent),
+                    colors = ButtonDefaults.buttonColors(Color(0xFF6C80E8))){
                     Text("Sign up")
                 }
             }
@@ -76,25 +88,42 @@ class LoginPage : Fragment() {
     }
 
     @Composable
+    fun Logo(){
+        Image(
+            painter = painterResource(id = R.drawable.paint_palette_svgrepo_com),
+            contentDescription = "",
+            contentScale = ContentScale.Fit
+        )
+    }
+
+    @Composable
     fun UsernameTextField(){
         var username by rememberSaveable { mutableStateOf("") }
 
         Row(verticalAlignment = Alignment.CenterVertically){
-            Icon(
-                Icons.Default.Person,
-                contentDescription = "login",
-                modifier = Modifier.size(35.dp))
-            Spacer(modifier = Modifier.width(7.dp))
+//            Icon(
+//                Icons.Default.Person,
+//                contentDescription = "login",
+//                modifier = Modifier.size(35.dp),
+//                tint = Color(0xFFC1C7D6))
+//            Spacer(modifier = Modifier.width(7.dp))
             TextField(
                 value = username,
                 onValueChange = { username = it },
                 label = { Text("Username") },
-                modifier = Modifier.border(border = BorderStroke(1.dp, Color.Black), shape = RoundedCornerShape(50.dp)),
+                modifier = Modifier.border(border = BorderStroke(1.dp, Color(0xFFC1C7D6)), shape = RoundedCornerShape(50.dp)),
                 colors = TextFieldDefaults.colors(
                     focusedIndicatorColor = Color.Transparent,
                     unfocusedIndicatorColor = Color.Transparent,
                     unfocusedContainerColor = Color.Transparent,
-                    focusedContainerColor = Color.Transparent)
+                    focusedContainerColor = Color.Transparent),
+                leadingIcon = {
+                    Icon(
+                        Icons.Default.Person,
+                        contentDescription = "login",
+                        modifier = Modifier.size(30.dp),
+                        tint = Color(0xFFC1C7D6))
+                }
 
             )
         }
@@ -105,43 +134,78 @@ class LoginPage : Fragment() {
         var password by rememberSaveable { mutableStateOf("") }
 
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Icon(
-                Icons.Default.Lock,
-                contentDescription = "login",
-                modifier = Modifier.size(35.dp))
-            Spacer(modifier = Modifier.width(7.dp))
+
+//            Spacer(modifier = Modifier.width(7.dp))
             TextField(
                 value = password,
                 onValueChange = { password = it },
                 label = { Text("Password") },
-                modifier = Modifier.border(border = BorderStroke(1.dp, Color.Black), shape = RoundedCornerShape(50.dp)),
+                modifier = Modifier.border(border = BorderStroke(1.dp, Color(0xFFC1C7D6)), shape = RoundedCornerShape(50.dp)),
                 colors = TextFieldDefaults.colors(
                     focusedIndicatorColor = Color.Transparent,
                     unfocusedIndicatorColor = Color.Transparent,
                     unfocusedContainerColor = Color.Transparent,
                     focusedContainerColor = Color.Transparent),
                 visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                leadingIcon = {
+                    Icon(
+                    Icons.Default.Lock,
+                    contentDescription = "login",
+                    modifier = Modifier.size(30.dp),
+                    tint = Color(0xFFC1C7D6))},
+
             )
         }
     }
 
-    @Preview(showBackground = true,  widthDp = 400, heightDp = 1000)
+    @Composable
+    fun Register(){
+        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
+            Text(
+                text = "Sign Up",
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+
+            UsernameTextField()
+            Spacer(modifier = Modifier.height(15.dp))
+            PasswordTextField()
+            Spacer(modifier = Modifier.height(15.dp))
+
+            Button(onClick = { },
+                colors = ButtonDefaults.buttonColors(Color(0xFF6C80E8))){
+                Text("Login")
+            }
+
+            Spacer(modifier = Modifier.height(75.dp))
+
+            Row(verticalAlignment = Alignment.CenterVertically){
+                Text("Don't have an account? ")
+                Spacer(modifier = Modifier.width(10.dp))
+                Button(onClick = { },
+                    modifier = Modifier.background(Color.Transparent),
+                    colors = ButtonDefaults.buttonColors(Color(0xFF6C80E8))){
+                    Text("Sign up")
+                }
+            }
+        }
+    }
+
+    @Preview(showBackground = true,  widthDp = 412, heightDp = 892)
     @Composable
     fun LoginPreview() {
             Login()
     }
 
-    @Preview(showBackground = true)
-    @Composable
-    fun TextFieldPreviews() {
-        UsernameTextField()
-        PasswordTextField()
-    }
-
-    @Preview(showBackground = true)
+    @Preview(showBackground = true,  widthDp = 412, heightDp = 892)
     @Composable
     fun RegisterPreview() {
-        Login()
+        Register()
     }
+
 }
